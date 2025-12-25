79b18bec105416aa3541bb25816368a934e5c9d9
Dsh219 <duan799628509@gmail.com>
2025-12-25 15:13:55 +0000

    update the hourly rules
    ttl is now set to 48 hrs, the daily AQI will be created after 2 days, since the daily data does not load at the same time, it is between 4 - 7 am,

--- lambda_hourly_orign.py
+++ lambda_hourly_orign.py
@@ -8,6 +8,14 @@ def lambda_handler(event, context):
     pmsensors = ["SDS011","SPS30","PMS5003","PMS7003",
         "PMS1003","HPM","PPD42NS","SDS021","PMS3003",
         "PMS6003","NEXTPM"]
+    PM25_RANGES = [ (11, 1), (23, 2), (35, 3), (41, 4), (47, 5), 
+               (53, 6), (58, 7), (64, 8), (70, 9), (float('inf'), 10) ]
+    PM10_RANGES = [ (16, 1), (33, 2), (50, 3), (58, 4), (66, 5), 
+                (75, 6), (83, 7), (91, 8), (100, 9), (float('inf'), 10) ]
+    def aqi(value:float, ranges:list) -> int:
+        for high, score in ranges:
+            if value <= high:
+                return score
     for t in range(5):  # Retry up to 5 times
         try:
             with requests.urlopen(url) as resp:
@@ -33,20 +41,20 @@ def lambda_handler(event, context):
                 dic["timestamp"] = f"{Each['timestamp'].replace(' ' ,'T')}" + "Z"
                 dic["type"] = Type
                 dic["PM10"] = 0
-                dic["PM2_5"] = 0
+                dic["PM2.5"] = 0
                 for measurement in Each['sensordatavalues']:
                     if measurement['value_type'] == "P1":
                         try:
-                            dic["PM10"] = float(measurement['value'])
+                            dic["PM10"] = aqi(float(measurement['value']), PM10_RANGES)
                         except Exception:
                             pass    
                     elif measurement['value_type'] == "P2":
                         try:
-                            dic["PM2_5"] = float(measurement['value'])
+                            dic["PM2.5"] = aqi(float(measurement['value']), PM25_RANGES)
                         except Exception:
                             pass
                 dic["altitude"] = Each['location'].get('altitude', "N/A")
-                dic["expires_at"] = int(time.time() + 3600*24*2)  # 48 hours TTL
+                dic["expires_at"] = int(time.time() + 3600*24)  # 24 hours TTL
                 batch.put_item(Item=dic)
                 num += 1
 
